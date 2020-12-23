f5eed4fd5efddae9fe1253dacb02e8876f2d7ddd
choepro577 <choepro57@gmail.com>
2020-12-23 16:34:37 +0700

    first commit

---
+++ testgit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testgit
+//
+//  Created by Nguyễn Đức Hiếu on 12/23/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
